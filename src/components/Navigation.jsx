da57534631b01e2cf7c615e8f547c2f13f1bf0bd
golukumar6299 <golunandansharma@gmail.com>
2024-01-20 15:10:17 +0530

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+    return (
+      <nav className="container">
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+  
+        <button className="logo-btn">login</button>
+      </nav>
+    );
+  };
+  
+  export default Navigation;
